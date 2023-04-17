57760250983b01e7d5d0c5b8602c5f06a3420d85
220SRS <jesikrjy@gmail.com>
2023-04-17 17:32:31 +0900

    backend setting

---
+++ backend/stackOverFlowClone/src/main/java/com/preproject/stackOverFlowClone/StackOverFlowCloneApplication.java
@@ -0,0 +1,13 @@
+package com.preproject.stackOverFlowClone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StackOverFlowCloneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StackOverFlowCloneApplication.class, args);
+	}
+
+}
