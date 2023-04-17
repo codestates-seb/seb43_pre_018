06c653d63eb31674bbc963542999034fcf2177de
rosevilleage <wkdcks125@naver.com>
2023-04-17 15:06:06 +0900

    [init] 프로젝트 초기설정

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -1,8 +1,31 @@
 import './App.css';
+import styled from 'styled-components'
 
+const TempHeader = styled.div`
+  border: 1px solid black;
+`
+const TempBody = styled.div`
+  border: 1px solid black;
+  display: flex;
+  >div {
+    border: 1px solid red;
+    margin: 0 3px;
+  }
+`
+const TempFooter = styled.div`
+  border: 1px solid black;
+`
 function App() {
   return (
-    <div><h1>프로젝트 시작</h1></div>
+    <div>
+      <TempHeader>Header</TempHeader>
+      <TempBody>
+        <div>Nav</div>
+        <div>Main</div>
+        <div>Side</div>
+      </TempBody>
+      <TempFooter>Footer</TempFooter>
+    </div>
   );
 }
 
