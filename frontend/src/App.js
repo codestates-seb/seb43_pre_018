79ce19252b3424db659d888a4543f5d34848088c
jungsoonhyun <bonoyah4@gmail.com>
2023-04-21 17:55:52 +0900

    헤더,로그인,회원가입완료

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,31 +1,27 @@
-import './App.css';
-import styled from 'styled-components'
+// import logo from "./logo.svg";
+import { ThemeProvider } from "styled-components";
+import "./App.css";
+import Header from "./Component/Header/Header";
+import theme from "./Component/style/theme";
+import { Routes, Route, BrowserRouter } from "react-router-dom";
+import Login from "./Component/Header/Login";
+import Signup from "./Component/Header/Signup";
+import PasswordPopup from "./Component/Header/PasswordPopup";
+import LogoHomeButton from "./Component/Header/LogoHomeButton";
 
-const TempHeader = styled.div`
-  border: 1px solid black;
-`
-const TempBody = styled.div`
-  border: 1px solid black;
-  display: flex;
-  >div {
-    border: 1px solid red;
-    margin: 0 3px;
-  }
-`
-const TempFooter = styled.div`
-  border: 1px solid black;
-`
 function App() {
   return (
-    <div>
-      <TempHeader>Header</TempHeader>
-      <TempBody>
-        <div>Nav</div>
-        <div>Main</div>
-        <div>Side</div>
-      </TempBody>
-      <TempFooter>Footer</TempFooter>
-    </div>
+    <ThemeProvider theme={theme}>
+      <BrowserRouter>
+        <Header />
+        <Routes>
+          <Route path="/header" element={<LogoHomeButton />}></Route>
+          <Route path="/login" element={<Login />}></Route>
+          <Route path="/signup" element={<Signup />}></Route>
+          <Route path="/passwordPopup" element={<PasswordPopup />}></Route>
+        </Routes>
+      </BrowserRouter>
+    </ThemeProvider>
   );
 }
 
---
+++ frontend/src/store/store.js
@@ -0,0 +1,27 @@
+// import { createSlice } from "@reduxjs/toolkit";
+
+// const isLoginSlice = createSlice({
+//   name: "isLogin",
+//   initialState: { value: false },
+//   reducers: {
+//     setIsLogin: (state, action) => {
+//       state.value = action.payload;
+//     },
+//   },
+// });
+
+// export const { setIsLogin } = isLoginSlice.actions;
+// export default isLoginSlice.reducer;
+
+// import { configureStore } from "@reduxjs/toolkit";
+// import logger from "redux-logger";
+// import authReducer from "./authSlice";
+
+// const store = configureStore({
+//   reducer: {
+//     auth: authReducer,
+//   },
+//   // middleware,
+// });
+
+// export default store;
