e79524eb9adac01540eaf535b0cd724344999dc1
nullpp00 <nullpp00@gmail.com>
2023-04-21 18:47:36 +0900

    /

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -40,6 +40,7 @@ function App() {
           <Route path="/" element={<Main />} />
           <Route path="/:postId" element={<QuestionDetail />} />
         </Routes>
+        <RightSidebar />
       </TempBody>
       <Footer />
     </BrowserRouter>
