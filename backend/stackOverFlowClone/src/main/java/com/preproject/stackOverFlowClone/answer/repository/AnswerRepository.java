ebb960ca113c6391d83305cf410db6dfc5f46a3c
220SRS <jesikrjy@gmail.com>
2023-04-22 20:55:50 +0900

    Feat : askId로 answer 조회 기능
    - askId로 answer 페이지네이션으로 조회하는 기능 추가

--- backend/stackOverFlowClone/src/main/java/com/preproject/stackOverFlowClone/answer/repository/AnswerRepository.java
+++ backend/stackOverFlowClone/src/main/java/com/preproject/stackOverFlowClone/answer/repository/AnswerRepository.java
@@ -5,8 +5,10 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
 import java.util.List;
-import java.util.Optional;
 
 public interface AnswerRepository extends JpaRepository<Answer, Long> {
 
@@ -18,4 +20,6 @@ public interface AnswerRepository extends JpaRepository<Answer, Long> {
 
     @Query("SELECT CASE WHEN COUNT(a) > 0 THEN true ELSE false END FROM Ask a WHERE a.id = :askId")
     boolean existsByAskId(@Param("askId") Long askId);
+
+    Page<Answer> findAllByAskId(Long askId, Pageable pageable);
 }
