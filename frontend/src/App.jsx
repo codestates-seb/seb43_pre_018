4be9acfc124c2029633c4e14b6a873a1ebb28972
rosevilleage <wkdcks125@naver.com>
2023-04-23 19:46:55 +0900

    Style : 무의미한 주석 삭제

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -6,8 +6,6 @@ import Login from "./conponents/Header/Login";
 import Signup from "./conponents/Header/Signup";
 import PasswordPopup from "./conponents/Header/PasswordPopup";
 import styled from 'styled-components'
-import Nav from './conponents/Nav';
-import RightSidebar from './conponents/RightSidebar';
 import Footer from './conponents/Footer';
 import AskQuestion from './page/AskQuestion';
 import Main from './conponents/Main';
@@ -22,27 +20,6 @@ const Top = styled.div`
   height: ${props=>props.isAsk?'100vh':'max-content'};
   width: 100%;
   background-color: ${props=>props.isAsk?'rgb(248, 249, 249)':'rgb(255,255,255)'};
-`
-
-const TempHeader = styled.div`
-  border: 1px solid black;
-  position: fixed;
-  top: 0;
-  width: 100vw;
-`
-// 높이 너비는 임시값
-const BodyContainer = styled.div`
-  margin: 33px auto 0;
-  height: 100vh;
-  min-height: calc(100vh - 378px);
-  max-width: 1264px;
-  padding: 20px 0 50px;
-  display: flex;
-  >.temp {
-    border: 1px solid red;
-    width: 751px;
-    height: 2000px;
-  }
 `
 
 function App() {
@@ -64,40 +41,5 @@ function App() {
     </Top>
   )
 }
-
-// function App() {
-//   return (
-//     <BrowserRouter>
-//       <ThemeProvider theme={theme}>
-//         <Header />
-//         <Routes>
-//           <Route path="/header" element={<LogoHomeButton />}></Route>
-//           <Route path="/login" element={<Login />}></Route>
-//           <Route path="/signup" element={<Signup />}></Route>
-//           <Route path="/passwordPopup" element={<PasswordPopup />}></Route>
-//         </Routes>
-//       </ThemeProvider>
-//     </BrowserRouter>
-//   );
-// }
-
-// function App() {
-//   const location = useLocation()
-//   return (
-//     <Top isAsk={location.pathname==='/ask'?true:false}>
-//       <TempHeader>Header</TempHeader>
-//       <Routes>
-//         <Route path='/' element={
-//           <TempBody>
-//             <Nav />
-//               <div className='temp'>Main</div>
-//             <RightSidebar />
-//           </TempBody>
-//         }/>
-//         <Route path='/ask' element={<AskQuestion />}/>
-//       </Routes>
-//       <Footer />
-//     </Top>  
-//   );
 
 export default App;
