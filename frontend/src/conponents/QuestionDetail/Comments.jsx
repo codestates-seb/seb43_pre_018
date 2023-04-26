305b04f46123ea5fe7cfb18ba1a28d7c24f42670
rosevilleage <wkdcks125@naver.com>
2023-04-27 03:21:55 +0900

    Feat : 댓글 삭제 기능 구현

--- frontend/src/conponents/QuestionDetail/Comments.jsx
+++ frontend/src/conponents/QuestionDetail/Comments.jsx
@@ -3,17 +3,25 @@ import { useRef } from "react";
 import { useState } from "react";
 import { useNavigate } from "react-router";
 import styled from "styled-components";
+import {FiEdit3} from "react-icons/fi"
+import { IconContext } from "react-icons/lib";
+import {RiDeleteBin2Line} from "react-icons/ri"
 
 const Container = styled.div`
-	
 	margin-top: 20px;
+`
+
+const SubContainer =styled.div`
+	display: flex;
+	justify-content: ${props=>props.edit?'space-between':'start'};
+	align-items: center;
+	border-top: ${props=>props.first?'2px solid rgb(241, 242, 243)':'1px solid rgb(241, 242, 243)'};
+	border-bottom: ${({first})=>first?'none':'1px solid rgb(241, 242, 243)'};
 `
 
 const CommentBox = styled.div`
 	display: flex;
 	flex-wrap: wrap;
-	border-top: ${props=>props.first?'2px solid rgb(241, 242, 243)':'1px solid rgb(241, 242, 243)'};
-	border-bottom: ${({first})=>first?'none':'1px solid rgb(241, 242, 243)'};
 	padding: 8px 0 8px 40px;
 	>span {
 		margin-right: 5px;
@@ -58,11 +66,21 @@ const AddForm = styled.div`
 	}
 `
 
+const IconContainer = styled.div`
+  justify-content: end;
+  >.edit-icon {
+    margin-left: 5px;
+    justify-self: start;
+}`
+
 export default function Comments({data, askId, answerId}) {
 	const [newContent, setNewContent] = useState('')
 	const [add, setAdd] = useState(false);
 	const inputRef = useRef([]);
 	const navigate = useNavigate()
+	const username = localStorage.getItem('name');
+	const url = process.env.REACT_APP_URL;
+	const token = localStorage.getItem('JWT')
 
 	const onChangeHandle = e => {
 		setNewContent(e.target.value)
@@ -133,19 +151,49 @@ export default function Comments({data, askId, answerId}) {
   //     .catch(e=>console.error(e.message))
 	// 	}
 	// }
+
+	const commentDelete = (commentId) => {
+    const headers = {
+      headers : {
+        "Access-Control-Allow-Origin": "*",
+        "Content-Type": "application/json",
+        "Authorization": token
+      }
+    }
+    axios.delete(`${url}/comment/${commentId}`, headers)
+    .then(res=>{
+      console.log('ansewr delete ok')
+      navigate(0)
+    })
+    .catch(e=>console.error(e.message))
+  }
+
 	return (
 		<Container>
 			{data.map((c,i)=>{
 				return (
-						<CommentBox 
-							key={c.commentId} 
-							first={i===0&&true}
-						>
+					<SubContainer 
+						edit={username===c.memberName}
+						key={c.commentId} 
+						first={i===0&&true}
+					>
+						<CommentBox>
 							<span>{c.content}</span>
 							<span>-</span>
 							<Author>{c.memberName}</Author>
 							<span>{c.createdAt}</span>
 						</CommentBox>
+						{username===c.memberName&&
+						<IconContainer>
+							<IconContext.Provider value={{size: '1.2rem', color: 'darkgray'}}>
+								<FiEdit3 className="edit-icon" />
+							</IconContext.Provider>
+							<IconContext.Provider value={{size: '1.2rem', color: 'rgb(235, 55, 39)'}}>
+								<RiDeleteBin2Line className="edit-icon" onClick={()=>commentDelete(c.commentId)}/>
+							</IconContext.Provider>
+						</IconContainer>
+						}
+					</SubContainer>
 				)
 			})}
 			{add? <InputForm>
